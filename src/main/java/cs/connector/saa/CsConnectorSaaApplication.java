89e1eff58ab2fa054c9070b545d078c9aec065a2
micnori <micnori@yahoo.com>
2021-05-27 11:20:07 +0200

    init

---
+++ src/main/java/cs/connector/saa/CsConnectorSaaApplication.java
@@ -0,0 +1,13 @@
+package cs.connector.saa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsConnectorSaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsConnectorSaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/connector/saa/CsConnectorSaaApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.connector.saa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsConnectorSaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
